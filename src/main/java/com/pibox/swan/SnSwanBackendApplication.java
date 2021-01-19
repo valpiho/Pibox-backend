71a20c5d1d4ed049c28cf9966f0cbaf6007d3fe5
Valentin Piho <val.piho@gmail.com>
2021-01-19 11:03:05 +0200

    Init

---
+++ src/main/java/com/pibox/swan/SnSwanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pibox.swan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnSwanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnSwanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pibox/swan/SnSwanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pibox.swan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnSwanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
