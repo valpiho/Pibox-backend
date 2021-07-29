8c74cc2d9a25edb8c0308822d6e5947a0f27fff8
Valentin Piho <val.piho@gmail.com>
2021-07-29 20:15:27 +0300

    Refactoring public urls

--- src/main/java/com/pibox/core/constant/SecurityConstant.java
+++ src/main/java/com/pibox/core/constant/SecurityConstant.java
@@ -11,7 +11,7 @@ public class SecurityConstant {
     public static final String FORBIDDEN_MESSAGE = "You need to login to access this page";
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
-    public static final String[] PUBLIC_URLS = { "/user/login", "/user/register", "/user/reset-password/*", "/user/image/**",
+    public static final String[] PUBLIC_URLS = { "/api/v1/user/login", "/api/v1/user/register", "/api/v1/user/reset-password/*", "/user/image/**",
             "/api-docs", "/swagger-config/**", "/v3/api-docs/**", "/swagger-ui/**"};
 
 }
